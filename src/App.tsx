786fe96169a814981b15a2d3793ad341c324b63c
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-21 00:35:50 +0530

    Removed - unnecessary files, and modified App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import React from "react";
 
-import './App.css';
-
-function App() {
-  return (
-    // <div className="App font-dm_sans">
-    //   Agile Shift
-    // </div>
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/signup" />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="*" element={<NoPage />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+	return <div>App</div>;
+};
 
 export default App;
