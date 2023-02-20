fddebd11ecc049b040450280f71b73b385c36a38
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-02-21 01:41:02 +0530

    Added types in global namespace

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -14,16 +14,41 @@ declare global{
         color:string;//hash code of color
         tagName:string;
     }
+    type TYPE_NOTIFICATION={
+        type:string;//issues, tickets, or parts.
+        id:string;//id of ticket or issues mentioned in update
+        message:string;
+        isRead:boolean;
+    }
+    type TYPE_VISTA={
+        [key:string]:[{}];//to be changed
+    }
+    type TYPE_TASK={
+        id:string;
+        taskName:string;
+    }
+    type TYPE_TASKS={
+        [key:string]:TYPE_TASK[];
+    }
     type TYPE_ORGANISATION={
         id:string;
         name:string;
         dateOfCreation:string;
         users:string[];
         profileImageUrl:string;
-        vista:{};//to be changed
+        vista:TYPE_VISTA;
         issues:TYPE_ISSUE[];//to be changed
         ticket:TYPE_TICKET[];//to be changed
         tags:TYPE_TAGS[];
-        parts:TYPE_PARTS[];
+        parts:TYPE_PARTS[];//to be changed
+        notifications:TYPE_NOTIFICATION[];
+        tasks:TYPE_TASKS;
+    }
+
+    type TYPE_USER={
+        id:string;
+        email:string;
+        avatar:string;//url of the avatar
+        organisation:string[];//array of organisation ids
     }
 }
