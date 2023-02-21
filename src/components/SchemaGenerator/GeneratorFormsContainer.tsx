90d123d741fc7ccad7d7c872bea521df628ce3f9
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-02-21 22:49:28 +0530

    added generator forms

---
+++ src/components/SchemaGenerator/GeneratorFormsContainer.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { SchemaGenerator } from "./SchemaGenerator";
+
+export const GeneratorFormsContainer = () => {
+  const [ticketColumnList, setTicketColumnList] = useState<
+    TYPE_TICKETS_SCHEMA[]
+  >([
+    { columnName: "Ticket Name", columnType: "string" },
+    { columnName: "Created By", columnType: "user" },
+    { columnName: "Tag", columnType: "tag" },
+  ]);
+  const [issueColumnList, setIssueColumnList] = useState<TYPE_ISSUES_SCHEMA[]>([
+    { columnName: "Ticket Name", columnType: "string" },
+    { columnName: "Created By", columnType: "user" },
+    { columnName: "Tag", columnType: "tag" },
+  ]);
+  const [partColumnList, setPartColumnList] = useState<TYPE_PARTS_SCHEMA[]>([
+    { columnName: "Ticket Name", columnType: "string" },
+    { columnName: "Created By", columnType: "user" },
+    { columnName: "Tag", columnType: "tag" },
+  ]);
+  return (
+    <div className="w-screen h-screen flex divide-x divide-dark_gray">
+      <SchemaGenerator
+        type="Tickets"
+        list={ticketColumnList}
+        setList={setTicketColumnList}
+      />
+      <SchemaGenerator
+        type="Issues"
+        list={issueColumnList}
+        setList={setIssueColumnList}
+      />
+      <SchemaGenerator
+        type="Parts"
+        list={partColumnList}
+        setList={setPartColumnList}
+      />
+    </div>
+  );
+};
