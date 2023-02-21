0d1c46e5641e7decb82abb9557ba7d8d19209c31
Satyam7Jha <7satyamjha@gmail.com>
2023-02-21 09:32:55 +0530

    backend-struct

---
+++ src/components/Utils/Backend.tsx
@@ -0,0 +1,11 @@
+// funcitons list
+
+export const check_users_database = () => {};
+export const create_user = () => {};
+export const create_organization = () => {};
+export const update_organization = () => {};
+export const create_ticket = () => {};
+export const create_issue = () => {};
+export const update_ticket = () => {};
+export const update_issue = () => {};
+export const create_tags = () => {};
