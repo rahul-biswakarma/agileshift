834fe6b90a29e3ff2a0088f58c2c9a9ca4e9ef5a
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-21 19:38:14 +0530

    Recording DataTable Component Progress

--- src/components/Dashboard/Index.tsx
+++ src/components/Dashboard/Index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import DataTable from "../DataTable";
 import NavBar from "./NavBar";
 
 export default function Dashboard() {
-  return (
-    <div className="bg-background_color h-[100vh]">
-      <header className="p-2 flex flex-wrap">
-        <NavBar />
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="bg-background_color h-[100vh]">
+				<header className="p-2 flex flex-wrap">
+					<NavBar />
+				</header>
+				<DataTable />
+			</div>
+		</>
+	);
 }
