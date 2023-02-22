e158a1958675b4a9a49fd4d1c919c06355839eeb
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-22 23:46:05 +0530

    Recording DataTable Progress"

--- src/components/Dashboard/Index.tsx
+++ src/components/Dashboard/Index.tsx
@@ -40,7 +40,7 @@ export default function Dashboard() {
 	];
 
 	return (
-		<div className="bg-background_color h-[100vh]">
+		<div className="bg-background_color h-[100vh] font-dm_sans">
 			<header className="p-2 flex flex-wrap text-primary_font_color justify-between gap-x-4 gap-y-2">
 				<NavBar
 					selectedTab={selectedTab}
@@ -50,13 +50,16 @@ export default function Dashboard() {
 			</header>
 			<DataTable
 				datas={dummyData}
-				dataSchema={[
-					{ columnTitle: "id", columnType: "string" },
-					{ columnTitle: "title", columnType: "string" },
-					{ columnTitle: "stage", columnType: "string" },
-					{ columnTitle: "owner", columnType: "string" },
-					{ columnTitle: "tag", columnType: "tag" },
-				]}
+				dataSchema={{
+					color: "purple",
+					schema: [
+						{ columnTitle: "id", columnType: "string" },
+						{ columnTitle: "title", columnType: "string" },
+						{ columnTitle: "stage", columnType: "string" },
+						{ columnTitle: "owner", columnType: "string" },
+						{ columnTitle: "tag", columnType: "tag" },
+					],
+				}}
 				feildColor="purple"
 			/>
 		</div>
