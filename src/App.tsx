e88dd6ac362f5269e8b95d694eaa58dcb68c51b2
saisravani20 <saisravani.koppala@gmail.com>
2023-02-22 14:58:02 +0530

    recording filter component updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,27 @@ import Dashboard from "./components/Dashboard/Index";
 import { Login } from "./components/OnBoarding/Login";
 import { SignUp } from "./components/OnBoarding/Signup";
 import OrganizationList from "./components/ManageOrganisation/OrganizationList";
+import Filter from "./components/Dashboard/Filter";
+
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Navigate to="/signup" />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-		<Route
-					path="/orglist"
-					element={<OrganizationList />}
-				/>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="" element={<Navigate to="/signup" />} />
+    //     <Route path="/signup" element={<SignUp />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+		// <Route
+		// 			path="/orglist"
+		// 			element={<OrganizationList />}
+		// 		/>
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+    <Filter/>
+  </div>
     
   );
 };
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,30 +1,11 @@
-import React, { useState } from "react";
+import React from 'react'
 
-type ColumnType = "string" | "user" | "tags";
-
-interface Column {
-  columnName: string;
-  columnType: ColumnType;
-}
-
-const initialData: Column[] = [
-  { columnName: "Ticket Name", columnType: "string" },
-  { columnName: "Created By", columnType: "user" },
-  { columnName: "Tag", columnType: "tags" },
-];
-
-const Filter: React.FC = () => {
-  const [columns, setColumns] = useState(initialData);
-
-  const handleColumnClick = (columnType: ColumnType) => {
-    console.log(`Clicked column with type ${columnType}`);
-  };
-
+function Filter() {
   return (
     <div className="w-screen h-screen bg-[#161616]">
-        <div className="flex justify-between mb-4 mx-9 pt-4">
+        <div className='flex justify-between mb-4 mx-9 pt-4'>
             <div className='flex flex-wrap'>
-                <div className="flex">
+                <div className='flex'>
                     <button className='mr-4'>                            
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" className=" w-6 h-6 icon icon-tabler icon-tabler-at" viewBox="0 0 24 24">
                                 <path stroke="none" d="M0 0h24v24H0z"/>
@@ -49,18 +30,19 @@ const Filter: React.FC = () => {
                             <hr className='border-default w-px h-full'></hr>
                     </div>
                 </div>
-                <div className="flex items-center gap-4">
-                    {columns.map((column, index) => (
-                        <button
-                        key={index}
-                        className=" rounded-md border border-1 border-[#808080] text-[#808080] text-sm px-4 py-2 "
-                        onClick={() => handleColumnClick(column.columnType)}
-                        >
-                        {column.columnName}
-                        </button>
-                    ))}
+                <div className='flex items-center'>
+                    <button className='rounded-md h-7 w-16 border border-1 border-[#808080] text-[#808080] text-sm mr-4'>Owner</button>
                 </div>
-                <div className='flex items-center justify-center ml-4 gap-4'>
+                <div className='flex items-center'>
+                    <button className='rounded-md h-7 w-16 border border-1 border-[#808080] text-[#808080] text-sm mr-4'>Priority</button>
+                </div>
+                <div className='flex items-center'>
+                    <button className='rounded-md h-7 w-16 border border-1 border-[#808080] text-[#808080] text-sm mr-4'>Rev Org</button>
+                </div>
+                <div className='flex items-center'>
+                    <button className='rounded-md h-7 w-16 border border-1 border-[#808080] text-[#808080] text-sm mr-4'>Severity</button>
+                </div>
+                <div className='flex items-center justify-center'>
                     <button className='flex items-center justify-center rounded-md w-7 h-7 border border-1 border-[#808080] text-[#808080] text-sm mr-4 '>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" className=" w-4 h-4 text-center" viewBox="0 0 24 24">
                         <path stroke="none" d="M0 0h24v24H0z"/>
@@ -68,14 +50,13 @@ const Filter: React.FC = () => {
                     </svg>
                     </button>
                 </div>
-                <div className='flex items-center gap-4'>
+                <div className='flex items-center'>
                     <button className='text-[#808080] text-sm '>Clear</button>
                 </div>  
-
             </div>
             <div className='flex items-center'>
-                <div className='flex hover:bg-[#49494D] rounded-md px-4 py-2'>
-                    <button className='rounded-md h-7 text-[#808080] text-sm font-bold '>
+                <div className='flex mr-4'>
+                    <button className='rounded-md h-7 text-[#808080] text-sm font-bold'>
                         Sort
                     </button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" className="w-4 h-4icon icon-tabler icon-tabler-chevron-down" viewBox="0 0 24 24">
@@ -83,7 +64,7 @@ const Filter: React.FC = () => {
                         <path d="m6 9 6 6 6-6"/>
                     </svg>                    
                 </div>
-                <div className='flex hover:bg-[#49494D] rounded-md px-4 py-2'>
+                <div className='flex mr-4'>
                     <button className='rounded-md h-7 text-[#808080] text-sm font-bold'>
                         Group
                     </button>
@@ -92,7 +73,7 @@ const Filter: React.FC = () => {
                         <path d="m6 9 6 6 6-6"/>
                     </svg>                    
                 </div>
-                <div className='flex  hover:bg-[#49494D] rounded-md px-4 py-2'>
+                <div className='flex mr-4'>
                     <button className='rounded-md h-7 text-[#808080] text-sm font-bold'>
                         Customize
                     </button>
@@ -102,15 +83,14 @@ const Filter: React.FC = () => {
                     </svg>                    
                 </div>
             </div>
+        
+
         </div>
-
+            
     </div>
+      
     
-  );
-};
+  )
+}
 
-export default Filter;
-
-
-
-
+export default Filter
