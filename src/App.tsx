3d9ad44e77b6d96bd5e18c87ae90abe686718a85
saisravani20 <saisravani.koppala@gmail.com>
2023-02-23 09:34:55 +0530

    recording filter component updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,32 @@ import { Login } from "./components/OnBoarding/Login";
 import { SignUp } from "./components/OnBoarding/Signup";
 import OrganizationList from "./components/ManageOrganization/OrganizationList";
 import { GeneratorFormsContainer } from "./components/SchemaGenerator/GeneratorFormsContainer";
+import  Filter from "./components/Dashboard/Filter";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Navigate to="/signup" />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/schema-generator" element={<GeneratorFormsContainer />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/orglist" element={<OrganizationList />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="" element={<Navigate to="/signup" />} />
+    //     <Route path="/signup" element={<SignUp />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/schema-generator" element={<GeneratorFormsContainer />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //     <Route path="/orglist" element={<OrganizationList />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <Filter schema={[
+          { columnTitle: "Title", columnType: "string" },
+          { columnTitle: "Stage", columnType: "string" },
+          { columnTitle: "User", columnType: "string" },
+          { columnTitle: "Tags", columnType: "tag" },
+          { columnTitle: "Severity", columnType: "string" },
+          { columnTitle: "Type", columnType: "string" },
+          { columnTitle: "Rev Org", columnType: "string" },
+          { columnTitle: "Part", columnType: "string" },
+        ]} />
+    </div>
   );
 };
 
