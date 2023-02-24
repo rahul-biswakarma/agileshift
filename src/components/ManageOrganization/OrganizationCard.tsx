2b7609d248093c9cbf695d2b1499c0d979c52fdb
Nikhil Tidke <nikhil.st8@gmail.com>
2023-02-24 08:01:00 +0530

    organizationList dynamic content loading, integration with redux store, removed warnings, removed bugs

--- src/components/ManageOrganization/OrganizationCard.tsx
+++ src/components/ManageOrganization/OrganizationCard.tsx
@@ -11,15 +11,14 @@ export const OrganizationCard = ({name, url, orgId}: organizationCardProps) => {
 
   return (
     <div className='w-full flex justify-between items-center p-2 text-highlight_font_color border border-background_color hover:border-white/10 rounded-lg'>
-        <div className='flex gap-2'>
-            <img src="https://app.devrev.ai/static/profile-circle-black.png" className='w-14 rounded-full' alt="" />
+        <div className='flex gap-2 items-center'>
+            <img src="https://app.devrev.ai/static/profile-circle-black.png" className='w-12 rounded-full' alt="" />
             <div>
-                <h3 className='text-lg'>{name}</h3>
-                <p className='text-md text-dark_gray'>https://app.devrev.ai/{url}</p>
+                <h3 className='text-sm'>{name}</h3>
+                <p className='text-xs text-dark_gray'>https://app.devrev.ai/{url}</p>
             </div>
         </div>
-        <button onClick={() => navigate(orgId)} className="w-16 h-10 bg-dark_gray text-center text-lg rounded-lg">
-            {/* Plus Icon */}
+        <button onClick={() => navigate(orgId)} className="w-16 h-10 bg-dark_gray text-center text-sm rounded-lg">
             Enter
         </button>
     </div>
--- src/components/ManageOrganization/OrganizationList.tsx
+++ src/components/ManageOrganization/OrganizationList.tsx
@@ -5,51 +5,43 @@ import { useAppSelector } from "../../redux/hooks";
 import { get_user_by_id } from "../../Utils/Backend";
 import {OrganizationCard} from "./OrganizationCard";
 import { get_organizations_details } from "../../Utils/Backend";
-// import UploadJSON from "./UploadJSON";
 
 const OrganizationList: React.FunctionComponent = () => {
 	const [user, setUser] = useState<any>();
-	const [organization, setOrganizations] = useState<any>([]);
+	const [organizations, setOrganizations] = useState<any>(undefined);
 	const organizationList  = useAppSelector((state) => state.auth.organisationList);
 	const userId  = useAppSelector((state) => state.auth.userId);
 
-	// useEffect(() => {
-	// 	const getUserObj = async () => {
-	// 		await get_user_by_id(userId).then((data) => {
-	// 			setUser(data);
-	// 		})
-	// 	}
-	// 	const getOrganizationsDetails = async () => {
-	// 		organizationList.map(async (orgId) => {
-	// 			const orgObject = await get_organizations_details(orgId);
-	// 			setOrganizations([
-	// 				...organization,
-	// 				orgObject
-	// 			])
-	// 		})
-	// 	}
-	// 	getOrganizationsDetails();
-	// 	getUserObj();
-	// });
+	useEffect(() => {
+		const getUserObj = async () => {
+			await get_user_by_id(userId).then((data) => {
+				setUser(data);
+			})
+		}
+
+		const orgList:any = []
+		organizationList.map(async (orgId) => {
+			const orgObject = await get_organizations_details(orgId.trim());
+			orgList.push(orgObject);
+		})
+		setOrganizations(orgList);
+		getUserObj();
+	},[organizationList, userId]);
 	
 	const navigate = useNavigate();
-	// console.log(organizationList);
-	console.log(user);
-	console.log(organization);
 
 	return (
 		<div className="bg-background_color h-screen w-screen flex items-center justify-center font-dm_sans">
 			<div className="w-[350px] flex flex-col gap-5">
 				<div className="text-highlight_font_color">
 					<h3 className="text-xl mb-2">Create or Join a AgileShift Org</h3>
-					{/* Change this to dynamic username */}
 					<p className="text-primary_font_color text-sm">We found following organizations that matches your email address - {user?.email}</p>  
 				</div>
 				<div className="text-highlight_font_color flex flex-col gap-5 my-5">
-					<h4 className="text-md">You AgileOrgs <span className="p-2 rounded-md bg-Secondary_background_color">{organization?.length}</span></h4>
+					<h4 className="text-md">You AgileOrgs <span className="p-2 rounded-md bg-Secondary_background_color">{organizations?.length}</span></h4>
 					{
-						organization.map((orgData:any) => {
-							return <OrganizationCard name={orgData?.data.name} orgId={orgData?.data.id} url={orgData?.data.id} />
+						organizations && organizations?.map((orgData:any, index:number) => {
+							return <OrganizationCard key={index} name={orgData?.name} orgId={orgData?.id} url={orgData?.id} />
 						})
 					}
 				</div>
@@ -62,7 +54,6 @@ const OrganizationList: React.FunctionComponent = () => {
 					Create new AgileOrg
 				</button>
 			</div>
-			{/* <UploadJSON schemaType="tickets"/> */}
 		</div>
 	);
 };
