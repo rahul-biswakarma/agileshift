c21515daa45dbcd2e687dcf44fbaf093c2cd0468
Nikhil Tidke <nikhil.st8@gmail.com>
2023-02-25 22:11:22 +0530

    Filter Component Added

---
+++ src/components/Filters/DisplayFilters.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react'
+
+type DisplayFiltersProps = {
+    type: string,
+    data: string[],
+    activeFilters: {[key:string]:string[]}
+    setActiveFilters: React.Dispatch<React.SetStateAction<{[key:string]: string[]}>>
+}
+
+const DisplayFilters = ({type, data, setActiveFilters, activeFilters} : DisplayFiltersProps) => {
+    const [filterData, setFilterData] = useState([{
+        name: "P0",
+        active: false
+    },
+    {
+        name: "P1",
+        active: false
+    },
+    {
+        name: "P2",
+        active: false
+    },
+    {
+        name: "P3",
+        active: false
+    }]);
+
+    const [activeFilterData, setActiveFilterData] = useState<{[key:string]:string[]}>({});
+
+    const modifyFilterActiveState = (index:number) => {
+        const newArray = [...filterData];
+        const activeFilterList = {...activeFilterData};
+        if(newArray[index].active === true){
+            newArray[index].active = false;   
+        }else if(newArray[index].active === false){
+            newArray[index].active = true;
+            let filters:any;
+            activeFilterList[type] ? filters = [...activeFilterList[type]] : filters = [];
+            filters.push(newArray[index].name);
+            activeFilterList[type] = filters;
+        }
+        
+        setFilterData(newArray);
+        setActiveFilterData(activeFilterList);
+    }
+
+    useEffect(() => {
+        setActiveFilters({...activeFilters, ...activeFilterData});
+    }, [activeFilterData])
+    
+  return (
+    <div>
+        <div className="absolute top-[110%] left-0 bg-primary_background_color w-48 rounded-xl p-1 border border-white/20 text-highlight_font_color">
+            <div className="flex flex-wrap gap-1 p-2 border-b border-white/10">
+                {filterData.filter((column) => column.active === true).map((column, index) => (
+                    <button
+                        key={index}
+                        className="inline-block font-fira_code rounded-md border border-dark_gray text-highlight_font_color text-xs py-1 px-2"
+                    >
+                    {column.name}
+                    </button>
+                ))}
+            </div>
+            <div className="flex items-center p-2 border-b border-white/10">
+                <input className="bg-background_color text-xs flex-1 outline-none text-highlight_font_color placeholder:text-white/20" placeholder="Search" type="text"/>
+            </div>
+            <div className="flex flex-col gap-1 mt-1 p-1">
+                {
+                    filterData.map((data, index) => {
+                        if(data.active === true){
+                            return (
+                                <div key={index} onClick={() => modifyFilterActiveState(index)} className="px-2 py-1 text-xs font-semibold bg-Secondary_background_color rounded-lg cursor-pointer border border-white/10">
+                                    {data.name}
+                                </div>
+                            )
+                        }else{
+                            return (
+                                <div key={index} onClick={() => modifyFilterActiveState(index)}  className="px-2 py-1 text-xs font-semibold hover:bg-Secondary_background_color rounded-lg cursor-pointer border border-transparent hover:border-white/10">
+                                    {data.name}
+                                </div>
+                            )
+                        }
+                    })
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DisplayFilters
--- src/components/Filters/Filter.tsx
+++ src/components/Filters/Filter.tsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
+import DisplayFilters from "./DisplayFilters";
 
 
 interface Type_FilterProps {
     schema: Array<TYPE_SCHEMA>;
+}
+
+type TYPE_ActiveFilters = {
+    [key:string]: string[]
+}
+
+type TYPE_ActiveFiltersDropdown = {
+    [key:string]: boolean
 }
 
 
@@ -11,8 +20,22 @@ const Filter = (props:Type_FilterProps) => {
 
     const [modifiedSchema, setModifiedSchema] = useState<TYPE_SCHEMA[]>([]);
     const [showAllFilters, setShowAllFilters] = useState<boolean>(false);
-    const handleColumnClick = (columnType: string) => {
-        console.log(`Clicked column with type ${columnType}`);
+    const [activeFilters, setActiveFilters] = useState<TYPE_ActiveFilters>({});
+    const [activeFiltersDropdown, setActiveFiltersDropdown] = useState<TYPE_ActiveFiltersDropdown>({});
+
+    const handleColumnClick = (columnTitle: string) => {
+        const newFilters = {...activeFiltersDropdown};
+        if(newFilters[columnTitle]){
+            newFilters[columnTitle] = !newFilters[columnTitle];
+        }else{
+            for(const key in newFilters){
+                if (newFilters.hasOwnProperty(key)) {
+                    newFilters[key] = false;
+                }
+            }
+            newFilters[columnTitle] = true;
+        }
+        setActiveFiltersDropdown(newFilters);
     };
 
     useEffect(()=>{
@@ -33,15 +56,25 @@ const Filter = (props:Type_FilterProps) => {
         updateSchema(schema);
     },[schema])
     
+    const modifyActiveState = (index:number) => {
+        const newArray = [...modifiedSchema];
+        if(newArray[index].active == "1"){
+            newArray[index].active = "0";   
 
-    // console.log(modifiedschema);
+        }else if(newArray[index].active == "0"){
+            newArray[index].active = "1";
+        }
+        setModifiedSchema(newArray);
+    }
 
-    // const activeColumns = modifiedschema.filter((column) => column.active );
+    useEffect(() => {
+        console.log(activeFilters);     
+    },[activeFilters])
 
   return (
     <div className="w-screen h-screen bg-[#161616]">
         <div className="flex justify-between mb-4 mx-9 pt-4">
-            <div className='flex flex-wrap gap-4 '>
+            <div className='flex flex-wrap gap-3'>
                 <div className="flex">
                     <button className='mr-4'>                            
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" className=" w-6 h-6 icon icon-tabler icon-tabler-at" viewBox="0 0 24 24">
@@ -67,21 +100,42 @@ const Filter = (props:Type_FilterProps) => {
                             <hr className='border-default w-px h-full'></hr>
                     </div>
                 </div>
-                
-                {modifiedSchema.filter((column) => column.active === "1").map((column, index) => (
-                    <button
-                        key={index}
-                        className="font-fira_code rounded-md border border-[#808080] text-[#808080] text-sm px-[10px] py-[2px] "
-                        onClick={() => handleColumnClick(column.columnType)}
-                    >
-                    {column.columnTitle}
-                    </button>
-                ))}
-                
 
-                {/* Plus Button */}
-                <div className='relative flex items-center justify-center ml-4 gap-4'>
-                    <button onClick={() => setShowAllFilters(!showAllFilters)} className='flex items-center justify-center rounded-md w-7 h-7 border border-1 border-[#808080] text-[#808080] text-sm mr-4 '>
+
+                <div className="relative flex items-center gap-2">
+                    {modifiedSchema.filter((column) => column.active === "1").map((column, index) => (
+                        <div>
+                            <div
+                                key={index}
+                                className="font-fira_code text-xs cursor-pointer rounded-md border border-dark_gray text-dark_gray px-2 py-1"
+                                onClick={() => handleColumnClick(column.columnTitle)}
+                            >
+                                <h4>
+                                    {column.columnTitle}
+                                </h4>
+                            </div>
+                        {
+                            activeFiltersDropdown[column.columnTitle] === true && (
+                                <div className="absolute top-[100%]">
+                                    <DisplayFilters activeFilters={activeFilters} type={column.columnTitle} data={[]} setActiveFilters = {setActiveFilters}/>
+                                </div>
+                            )
+                        }
+                        {/* <div>
+                            {
+                                activeFilters[column.columnTitle] && activeFilters[column.columnTitle].map((data) => (
+                                    <span>
+                                        {data}
+                                    </span>
+                                ))
+                            }
+                        </div> */}
+                        </div>
+                    ))}
+                </div>
+                
+                <div className='relative flex items-center justify-center gap-4'>
+                    <button onClick={() => setShowAllFilters(!showAllFilters)} className='flex items-center justify-center rounded-md w-6 h-6 border border-1 border-[#808080] text-[#808080] text-sm mr-4 '>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" strokeLinejoin="round" stroke-width="1.5" className=" w-4 h-4 text-center" viewBox="0 0 24 24">
                             <path stroke="none" d="M0 0h24v24H0z"/>
                             <path d="M12 5v14m-7-7h14"/>
@@ -89,20 +143,39 @@ const Filter = (props:Type_FilterProps) => {
                     </button>
                     {
                         showAllFilters && (
-                            <div className="absolute top-[110%] left-0 bg-slate-500 h-96 w-96">
-                                {
-                                    modifiedSchema.map((data, index) => {
-                                        if(data.active === "1"){
-                                            return (
-                                                <></>
-                                            )
-                                        }else{
-                                            return (
-                                                <></>
-                                            )
-                                        }
-                                    })
-                                }
+                            <div className="absolute top-[110%] left-0 bg-primary_background_color w-48 rounded-xl p-1 border border-white/20 text-highlight_font_color">
+                                <div className="flex flex-wrap gap-1 p-2 border-b border-white/10">
+                                    {modifiedSchema.filter((column) => column.active === "1").map((column, index) => (
+                                        <button
+                                            key={index}
+                                            className="inline-block font-fira_code rounded-md border border-dark_gray text-highlight_font_color text-xs py-1 px-2"
+                                        >
+                                        {column.columnTitle}
+                                        </button>
+                                    ))}
+                                </div>
+                                <div className="flex items-center p-2 border-b border-white/10">
+                                    <input className="bg-background_color text-xs flex-1 outline-none text-highlight_font_color placeholder:text-white/20" placeholder="Search" type="text"/>
+                                </div>
+                                <div className="flex flex-col gap-1 mt-1 p-1">
+                                    {
+                                        modifiedSchema.map((data, index) => {
+                                            if(data.active === "1"){
+                                                return (
+                                                    <div key={index} onClick={() => modifyActiveState(index)} className="px-2 py-1 text-xs font-semibold bg-Secondary_background_color rounded-lg cursor-pointer border border-white/10">
+                                                        {data.columnTitle}
+                                                    </div>
+                                                )
+                                            }else{
+                                                return (
+                                                    <div key={index} onClick={() => modifyActiveState(index)}  className="px-2 py-1 text-xs font-semibold hover:bg-Secondary_background_color rounded-lg cursor-pointer border border-transparent hover:border-white/10">
+                                                        {data.columnTitle}
+                                                    </div>
+                                                )
+                                            }
+                                        })
+                                    }
+                                </div>
                             </div>
                         )
                     }
@@ -110,13 +183,13 @@ const Filter = (props:Type_FilterProps) => {
                
 
                 <div className='flex items-center gap-4'>
-                    <button className='text-[#808080] text-sm '>Clear</button>
+                    <button className='rounded-md h-7 text-[#808080] text-sm font-bold '>Clear</button>
                 </div>  
 
             </div>
             <div className='flex items-center font-dm_sans'>
                 <div className='flex hover:bg-[#49494D] rounded-md px-4 py-2'>
-                    <button className='rounded-md h-7 text-[#808080] text-sm font-bold '>
+                    <button className='rounded-md h-7 text-[#808080] text-sm font-bold'>
                         Sort
                     </button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#808080" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" className="w-4 h-4icon icon-tabler icon-tabler-chevron-down" viewBox="0 0 24 24">
