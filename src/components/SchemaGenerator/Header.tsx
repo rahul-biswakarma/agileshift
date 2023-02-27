ffa7de5ddf6eb411ca1295a08c5678d78a793a11
Rahul Biswakarma <rahul.id39@gmail.com>
2023-02-28 01:29:53 +0530

    Update. updated logic for close button

--- src/components/SchemaGenerator/Header.tsx
+++ src/components/SchemaGenerator/Header.tsx
@@ -14,14 +14,12 @@ const SchemaGeneratorFormHeader = () => {
 	return (
 		<div className="w-full h-[40px] border-b border-dark_gray flex justify-between items-center text-white/50 font-dm_sans font-[500] text-sm px-[1rem]">
 			<p>Organization Schema Form</p>
-			{useAppSelector((state) => state.schema.isEdit) === true && (
-				<span
-					onClick={() => navigateToDashboard()}
-					className="material-symbols-outlined hover:text-rose-400 cursor-pointer text-[17px] font-bold"
-				>
-					close
-				</span>
-			)}
+			<span
+				onClick={() => navigateToDashboard()}
+				className="material-symbols-outlined hover:text-rose-400 cursor-pointer text-[17px] font-bold"
+			>
+				close	
+			</span>
 		</div>
 	);
 };
