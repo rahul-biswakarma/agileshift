b793a2437726465a92597b05870598f2e0a8f9aa
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-03-05 02:51:49 +0530

    Fix. Resolved linkage reset in edit schema

--- src/components/SchemaGenerator/GeneratorFormsContainer.tsx
+++ src/components/SchemaGenerator/GeneratorFormsContainer.tsx
@@ -10,40 +10,44 @@ import { setActiveTab } from "../../redux/reducers/SchemaSlice";
 import SchemaGeneratorFormHeader from "./Header";
 import { toast } from "react-toastify";
 import TabsContainer from "./TabsContainer";
+import { LinkableForms } from "./LinkableForms";
 
+type GeneratorContainerPropTypes = {
+  mode: string;
+};
 
-type GeneratorContainerPropTypes={
-  mode:string;
-}
+export const GeneratorFormsContainer = ({
+  mode,
+}: GeneratorContainerPropTypes) => {
+  const navigate = useNavigate();
+  const dispatch: any = useAppDispatch();
+  const userId = useAppSelector((state: RootState) => state.auth.userId);
 
-export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
-	const navigate = useNavigate();
-	const dispatch:any = useAppDispatch();
-	const userId = useAppSelector((state:RootState) => state.auth.userId);
-
-	useEffect(() => {
-		if(!userId){
-			navigate('/login')
-		}
-	}, [navigate, userId]);	
+  useEffect(() => {
+    if (!userId) {
+      navigate("/login");
+    }
+  }, [navigate, userId]);
 
   const orgId = useAppSelector((state) => state.auth.organisationId);
 
-  useEffect(()=>{
-    if(orgId && mode === "edit"){
-      get_schema_data(orgId).then((data)=> {
-        if(data){console.log(data.schemaData); setFields(data.schemaData)};
-      })
+  useEffect(() => {
+    if (orgId && mode === "edit") {
+      get_schema_data(orgId).then((data) => {
+        if (data) {
+          console.log(data.schemaData);
+          setFields(data.schemaData);
+        }
+      });
     }
-  }, [orgId,mode])
-
+  }, [orgId, mode]);
 
   let activeTab = useAppSelector((state: RootState) => state.schema.activeTab);
 
-  if(mode === "edit" && activeTab === -1){
+  if (mode === "edit" && activeTab === -1) {
     dispatch(setActiveTab(0));
   }
-  
+
   const makeActualCopy = (columnList: TYPE_SCHEMA[]): TYPE_SCHEMA[] => {
     let newColumnList: TYPE_SCHEMA[] = [];
     for (let column of columnList) {
@@ -51,35 +55,37 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
     }
     return newColumnList;
   };
-  const defaultColumnList=useMemo<TYPE_SCHEMA[]>(()=>[
-    { columnName: "Name", columnType: "title" },
-    { columnName: "Tag", columnType: "tag" },
-    { columnName: "Owner", columnType: "user" },
-    { columnName: "Deadline", columnType: "date" },
-  ],[]);
+  const defaultColumnList = useMemo<TYPE_SCHEMA[]>(
+    () => [
+      { columnName: "Name", columnType: "title" },
+      { columnName: "Tag", columnType: "tag" },
+      { columnName: "Owner", columnType: "user" },
+      { columnName: "Deadline", columnType: "date" },
+    ],
+    []
+  );
 
-  const defaultFields = useMemo<TYPE_FIELD[]>(() => [
-    {
-      list: makeActualCopy(defaultColumnList),
-      name: "Tickets",
-      color: "purple",
-      icon: "home",
-      linkage: ["Tickets"],
-    },
-    {
-      list: makeActualCopy(defaultColumnList),
-      name: "Issues",
-      color: "cyan",
-      icon: "home",
-      linkage: ["Tickets","Issues"],
-    },
-  ], [defaultColumnList]);
-
+  const defaultFields = useMemo<TYPE_FIELD[]>(
+    () => [
+      {
+        list: makeActualCopy(defaultColumnList),
+        name: "Tickets",
+        color: "purple",
+        icon: "home",
+        linkage: [],
+      },
+      {
+        list: makeActualCopy(defaultColumnList),
+        name: "Issues",
+        color: "cyan",
+        icon: "home",
+        linkage: [],
+      },
+    ],
+    [defaultColumnList]
+  );
 
   // const [activeTab, dispatch(setActiveTab(] = useState(-1));
-
-
-
 
   const [fields, setFields] = useState<TYPE_FIELD[]>(defaultFields);
 
@@ -114,9 +120,11 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
   // }
 
   function changeLinkage(this: any, link: string[]) {
-  	let tempFields = [...fields];
-  	tempFields[this.id].linkage = link;
-  	setFields(tempFields);
+    console.log("link", link);
+    let tempFields = [...fields];
+    tempFields[this.id].linkage = link;
+    console.log("tempFields", tempFields);
+    setFields(tempFields);
   }
 
   // function removeLinkage(this: any, link: string) {
@@ -137,15 +145,14 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
       toast("Schema Created Successfully");
       navigate(`/organization/${organisationId}`);
     }, 1000);
-    
   };
 
   const addSchema = () => {
     let tempFields = [...fields];
     let lastField = tempFields[tempFields.length - 1];
     if (lastField.name === "") {
-      toast.warning("Please fill the name first",{
-        className: 'toast-message'
+      toast.warning("Please fill the name first", {
+        className: "toast-message",
       });
       return;
     }
@@ -164,9 +171,9 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
   function duplicateSchema(this: any) {
     let currentField = fields[this.id];
     if (currentField.name === "") {
-      toast.error("Cannot duplicate a schema with no name!")
-      return
-    };
+      toast.error("Cannot duplicate a schema with no name!");
+      return;
+    }
     let newField: TYPE_FIELD = {
       name: `${currentField.name} Duplicate`,
       list: makeActualCopy(currentField.list),
@@ -179,7 +186,7 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
       .concat(newField, fields.slice(this.id + 1));
     setFields(duplicatedArray);
     dispatch(setActiveTab(activeTab + 1));
-    toast.success("Schema duplicated successfulyy")
+    toast.success("Schema duplicated successfulyy");
   }
 
   function deleteSchema(this: any) {
@@ -200,21 +207,40 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
     tempFields[this.id].icon = icon;
     setFields(tempFields);
   }
+  const checkSchema = () => {
+    const fieldName = getAllFieldsName();
+    console.log(fieldName);
+
+    for (let i = 0; i < fieldName.length; i++) {
+      if (fieldName[i].length === 0) {
+        toast.error(`Field name cannot be empty`);
+        return;
+      }
+      for (let j = i + 1; j < fieldName.length; j++) {
+        if (fieldName[i].toLowerCase() === fieldName[j].toLowerCase()) {
+          toast.error(`Duplicate field name detected: ${fieldName[i]}`);
+          return;
+        }
+      }
+    }
+
+    submitSchema();
+  };
 
   useEffect(() => {
-    if(mode==="create"){
-      setFields(defaultFields)
-      dispatch(setActiveTab(-1))
+    if (mode === "create") {
+      setFields(defaultFields);
+      dispatch(setActiveTab(-1));
     }
-  }, [defaultFields, mode,dispatch]);
+  }, [defaultFields, mode, dispatch]);
 
   return (
     <div className="flex flex-col max-h-screen">
       <SchemaGeneratorFormHeader />
       <div className="relative w-screen h-[calc(100vh-40px)] flex">
-        <TabsContainer fields={fields} addSchema={addSchema} mode={mode}/>
-        {mode==="create" && <OrganisationForm mode={mode} />}
-        {fields.map((field:TYPE_FIELD, id:number) => (
+        <TabsContainer fields={fields} addSchema={addSchema} mode={mode} />
+        {mode === "create" && <OrganisationForm mode={mode} />}
+        {fields.map((field: TYPE_FIELD, id: number) => (
           <SchemaGenerator
             id={id}
             name={field.name}
@@ -223,7 +249,6 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
             setList={changeList.bind({ id: id })}
             getAllFieldsName={getAllFieldsName}
             key={id}
-            submitSchema={submitSchema}
             duplicateSchema={duplicateSchema.bind({ id: id })}
             deleteSchema={deleteSchema.bind({ id: id })}
             color={field.color}
@@ -231,10 +256,15 @@ export const GeneratorFormsContainer = ({mode}:GeneratorContainerPropTypes) => {
             icon={field.icon}
             changeIcon={changeIcon.bind({ id: id })}
             linkage={field.linkage}
-            changeLinkage={changeLinkage.bind({id:id})}
+            changeLinkage={changeLinkage.bind({ id: id })}
             mode={mode}
           />
         ))}
+        <LinkableForms
+          fields={fields}
+          changeLinkage={changeLinkage}
+          checkSchema={checkSchema}
+        />
       </div>
     </div>
   );
--- src/components/SchemaGenerator/TabsContainer.tsx
+++ src/components/SchemaGenerator/TabsContainer.tsx
@@ -1,23 +1,42 @@
 import React from 'react'
-import { NewSchema } from './NewSchema'
-import Tab from './Tab'
+import { useAppDispatch } from "../../redux/hooks";
+import { setActiveTab } from "../../redux/reducers/SchemaSlice";
+import { TabButton } from "./TabButton";
+import Tab from "./Tab";
 
 type TabsContainerPropTypes = {
-  fields:TYPE_FIELD[];
-  addSchema:()=>void;
-  mode:string;
-}
+  fields: TYPE_FIELD[];
+  addSchema: () => void;
+  mode: string;
+};
 
-export const TabsContainer = ({fields,addSchema,mode}:TabsContainerPropTypes) => {
+export const TabsContainer = ({
+  fields,
+  addSchema,
+  mode,
+}: TabsContainerPropTypes) => {
+  const dispatch = useAppDispatch();
   return (
-    <nav className='h-screen w-1/5 bg-Secondary_background_color border-r border-white/10 flex flex-col items-center'>
-     {mode==="create"&& <Tab organisation={true} id={-1}/>}
-      {fields.map((field:any, id:any) => (
-        <Tab key={id} field={field} id={id}/>
-      ))}
-        <NewSchema addSchema={addSchema} />
+    <nav className="h-full w-1/5 bg-Secondary_background_color border-r border-white/10 flex flex-col justify-between items-center py-4">
+      <div className="w-[95%] flex flex-col items-center justify-center gap-2">
+        {mode === "create" && <Tab organisation={true} id={-1} />}
+        <div className="w-full max-h-full flex flex-col overflow-auto gap-2">
+          {fields.map((field: TYPE_FIELD, id: number) => (
+            <Tab key={id} field={field} id={id} />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-[95%] flex flex-col">
+        <TabButton onClick={addSchema} text={"+ Add New Schema"} />
+        <TabButton
+          onClick={() => dispatch(setActiveTab(100))}
+          text={"Edit Linkables"}
+          id={100}
+        />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default TabsContainer
