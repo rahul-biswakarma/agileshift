4e7efc302ed9f71195b93f4d3f9084641d95af90
Nikhil Tidke <nikhiltidke@Nikhils-MacBook-Pro-2.local>
2023-03-13 10:56:33 +0530

    Add: Cypress testcase for schemaUpdation

--- cypress/e2e/schemaUpdate.cy.ts
+++ cypress/e2e/schemaUpdate.cy.ts
@@ -7,32 +7,32 @@ describe("Schema Editing Component Tests", () => {
     
     it("go to edit schema component", () => {
         // Create a new Column
-        // cy.getByData("edit-schema").should("exist").click();
-        // cy.getByData("add-new-schema").should("exist").click();
-        // cy.getByData("case-name-input").should("exist").type("Bugs");
-        // cy.getByData("edit-linkables").should("exist").click();
-        // cy.getByData("submit-schema").should("exist").click();
-        // cy.getByData("Bugs").should("exist");
+        cy.getByData("edit-schema").should("exist").click();
+        cy.getByData("add-new-schema").should("exist").click();
+        cy.getByData("case-name-input").should("exist").type("Bugs");
+        cy.getByData("edit-linkables").should("exist").click();
+        cy.getByData("submit-schema").should("exist").click();
+        cy.getByData("Bugs").should("exist");
 
         // Update a column schema in Org Schema
         cy.getByData("edit-schema").should("exist").click();
         cy.getByData("schema-tab-Bugs").should("exist").click();
         cy.getByData("add-column-btn").should("exist").click();
-        // cy.getByData("delete-schema-btn").should("exist").click();
-        // cy.getByData("handle-delete-confirm").should("exist").click();
-        // cy.getByData("edit-linkables").should("exist").click();
-        // cy.getByData("submit-schema").should("exist").click();
-        // cy.getByData("Bugs").should("not.exist");
+        cy.getByData("delete-schema-btn").should("exist").click();
+        cy.getByData("handle-delete-confirm").should("exist").click();
+        cy.getByData("edit-linkables").should("exist").click();
+        cy.getByData("submit-schema").should("exist").click();
+        cy.getByData("Bugs").should("not.exist");
 
 
         // Delete a Column from schema
-        // cy.getByData("edit-schema").should("exist").click();
-        // cy.getByData("schema-tab-Bugs").should("exist").click();
-        // cy.getByData("delete-schema-btn").should("exist").click();
-        // cy.getByData("handle-delete-confirm").should("exist").click();
-        // cy.getByData("edit-linkables").should("exist").click();
-        // cy.getByData("submit-schema").should("exist").click();
-        // cy.getByData("Bugs").should("not.exist");
+        cy.getByData("edit-schema").should("exist").click();
+        cy.getByData("schema-tab-Bugs").should("exist").click();
+        cy.getByData("delete-schema-btn").should("exist").click();
+        cy.getByData("handle-delete-confirm").should("exist").click();
+        cy.getByData("edit-linkables").should("exist").click();
+        cy.getByData("submit-schema").should("exist").click();
+        cy.getByData("Bugs").should("not.exist");
     });
 });
 slowCypressDown();
